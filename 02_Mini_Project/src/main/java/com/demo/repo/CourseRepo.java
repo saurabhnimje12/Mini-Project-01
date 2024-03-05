ac843d9294f4d60a33bdb62dc7358e6847a9825a
saurabhnimje12 <130309221+saurabhnimje12@users.noreply.github.com>
2024-03-05 16:41:06 +0530

    Delete 02_Mini_Project directory

--- 02_Mini_Project/src/main/java/com/demo/repo/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.entity.CourseEntity;
-
-public interface CourseRepo extends JpaRepository<CourseEntity, Integer> {
-
-}
